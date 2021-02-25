9596547a9c9e6e28e84c41a21e1f27357806302d
JakubManka <49609640+JakubManka@users.noreply.github.com>
2021-02-25 18:15:53 +0100

    Update Data.java

--- app/src/main/java/com/example/mylastpassphone/Data.java
+++ app/src/main/java/com/example/mylastpassphone/Data.java
@@ -2,13 +2,47 @@ package com.example.mylastpassphone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Data extends AppCompatActivity {
+
+    ArrayAdapter<String> dataArrayAdapter;
+    String url;
+    String username;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+
+        ListView dataListView = findViewById(R.id.dataList);
+        List<String> dataList = new ArrayList<>();
+
+        Button back = findViewById(R.id.back);
+
+
+        url = "url:";
+        username = "username:";
+        password = "password:";
+
+        dataList.add(url);
+        dataList.add(username);
+        dataList.add(password);
+
+        dataArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dataList);
+        dataListView.setAdapter(dataArrayAdapter);
     }
 }
